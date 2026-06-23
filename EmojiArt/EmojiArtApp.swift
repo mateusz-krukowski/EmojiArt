920b2036c70d925c124003d89e147dfb2aa966da
Mateusz Krukowski <mateusz_krukowski@yahoo.com>
2026-06-23 23:45:19 +0200

    Initial Commit

---
+++ EmojiArt/EmojiArtApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiArtApp.swift
+//  EmojiArt
+//
+//  Created by Mateusz on 23/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct EmojiArtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
